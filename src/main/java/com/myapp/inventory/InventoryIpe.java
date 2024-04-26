880470e0635dd48b0ce79ae8a20d499d3c0d1cdb
ven_chiheng <163480020+venchiheng@users.noreply.github.com>
2024-04-26 09:10:58 +0700

    first commit

---
+++ src/main/java/com/myapp/inventory/InventoryIpe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myapp.inventory;
+
+/**
+ *
+ * @author MSI-PC
+ */
+public class InventoryIpe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
